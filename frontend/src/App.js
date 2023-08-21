1fa80df6d67059148042491beb29d7e878772680
juliovianadev <juliovianadev@gmail.com>
2023-08-21 10:43:26 -0300

    subindo o projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Simples from "./components/exemplos/Simples"
+import Composto from "./components/exemplos/Composto"
+function App(){
+  return(
+    <div>
+      <h1>Tomate</h1>
+      <Simples />
+      <Composto />
+    </div>
+  )
+}
+
+export default App
